53f563191cf5b386127c3098a4f7b346533eaa41
matheswarirevathi <47688960+matheswarirevathi@users.noreply.github.com>
2019-03-22 08:26:17 -0700

    Create numbers.java

---
+++ numbers.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class numbers
+{
+	public static void main (String[] args)
+	{
+		int n=5;
+		if(n>0)
+		{
+			System.out.println(n+"is positive");
+		}
+		else if(n<0)
+		{System.out.println(n+"is negative");}
+		else
+		{
+			System.out.println(n+"is zero");
+		}
+	}
+}
