e1888a9f430b7b351ec8ee3626eac77b8fb57c4e
matheswarirevathi <47688960+matheswarirevathi@users.noreply.github.com>
2019-03-22 08:47:01 -0700

    Create oddoreven.java

---
+++ oddoreven.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class oddoreven
+{
+	public static void main(String args[]){
+		int n=24;
+		if(n%2==0)
+		System.out.println(n+"is even");
+		else
+		System.out.println(n+"is odd");
+	}
+}
